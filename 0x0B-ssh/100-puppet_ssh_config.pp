a0d3a129ca58655287ba2a3aa1b29bbdff78205f
wisdomwest <harmonykoome@gmail.com>
2023-11-25 01:58:43 -0800

    revert

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -4,12 +4,10 @@ file_line { 'private key only' :
     ensure => present,
     path   => '/etc/ssh/ssh_config',
     line   => 'IdentityFile ~/.ssh/school'
-    match  => '#IdentityFile'
 }
 
 file_line { 'refuse password' :
     ensure => present,
     path   => '/etc/ssh/ssh_config',
     line   => 'PasswordAuthentication no'
-    match  => '#PasswordAuthentication'
 }
