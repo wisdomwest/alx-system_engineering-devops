50308def89b5e9a25fbc78d62abf622992e42451
wisdomwest <harmonykoome@gmail.com>
2024-01-02 18:28:43 +0300

    task 3 same as task 2 but getting all users tasks and saving with format to json file

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
 """Use API to get data and print"""
+import json
 import requests
-import sys
 
 if __name__ == "__main__":
-    id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/"
 
     user_data = requests.get(url + "users").json()
-    todos_data = requests.get(url + "todos", params={"userId": id}).json()
+    id_tasks = {}
 
     with open("todo_all_employees.json", "w") as json_file:
         for user in user_data:
+            id = user.get("id")
+            todos_data = requests.get(
+                url + "todos", params={"userId": id}).json()
+            tasks = []
             for task in todos_data:
-                json.dump({
-                    user.get("id"): [{
-                        "username": user.get("username"),
-                        "task": task.get("title"),
-                        "completed": task.get("completed"),
-                    }]
-                }, json_file)
+                tasks.append({
+                    "username": user.get("username"),
+                    "task": task.get("title"),
+                    "completed": task.get("completed")
+                })
+
+            id_tasks[id] = tasks
+
+        json.dump(id_tasks, json_file)
