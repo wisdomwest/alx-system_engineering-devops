c21bafa948718efb11361cd26176521833d15a86
wisdomwest <harmonykoome@gmail.com>
2024-01-02 17:16:52 +0300

    task 1 learnt json to csv very intresting

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""Use API to get data and export to CSV format"""
+import csv
+import requests
+import sys
+
+if __name__ == "__main__":
+    id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+
+    user_data = requests.get(url + "users/{}".format(id)).json()
+    todos_data = requests.get(url + "todos", params={"userId": id}).json()
+
+    with open("{}.csv".format(id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+
+        for task in todos_data:
+            writer.writerow([
+                id,
+                user_data.get("username"),
+                task.get("completed"),
+                task.get("title")
+            ])
