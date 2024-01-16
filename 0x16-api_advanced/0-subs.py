d68c85271c02889b9912cff514a3c53225de9831
wisdomwest <harmonykoome@gmail.com>
2024-01-16 13:19:15 +0300

    added false redirects

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,8 @@ def number_of_subscribers(subreddit):
     """queries the Reddit API"""
     about = requests.get("https://www.reddit.com/r/{}/about.json"
                          .format(subreddit),
-                         headers={"User-Agent": "west"})
+                         headers={"User-Agent": "west"},
+                         allow_redirects=False)
     if about.status_code == 200:
         return about.json().get("data").get("subscribers")
     else:
