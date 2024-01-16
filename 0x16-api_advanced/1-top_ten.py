2c2842305fd5b1a47106b6d91731fd9cc11faded
wisdomwest <harmonykoome@gmail.com>
2024-01-16 14:19:47 +0300

    task 2 traversing json data lol

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python3
+"""queries the Reddit API and returns titles 
+    of first 10 hot posts for a given subreddit"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """queries the Reddit API"""
+    hot = requests.get("https://www.reddit.com/r/{}/hot.json"
+                         .format(subreddit),
+                         headers={"User-Agent": "west"},
+                         params={"limit": 10},
+                         allow_redirects=False)
+    if hot.status_code == 200:
+        for data in hot.json().get("data").get("children"):
+            children = data.get("data")
+            title = children.get("title")
+            print(title)
+    else:
+        print(None)
