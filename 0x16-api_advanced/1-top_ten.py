cb1a0c7e0e23d1a70b577ca40695934be92a29a0
wisdomwest <harmonykoome@gmail.com>
2024-01-16 14:21:34 +0300

    fixed pycode errors

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""queries the Reddit API and returns titles 
+"""queries the Reddit API and returns titles
     of first 10 hot posts for a given subreddit"""
 
 import requests
@@ -8,10 +8,10 @@ import requests
 def top_ten(subreddit):
     """queries the Reddit API"""
     hot = requests.get("https://www.reddit.com/r/{}/hot.json"
-                         .format(subreddit),
-                         headers={"User-Agent": "west"},
-                         params={"limit": 10},
-                         allow_redirects=False)
+                       .format(subreddit),
+                       headers={"User-Agent": "west"},
+                       params={"limit": 10},
+                       allow_redirects=False)
     if hot.status_code == 200:
         for data in hot.json().get("data").get("children"):
             children = data.get("data")
