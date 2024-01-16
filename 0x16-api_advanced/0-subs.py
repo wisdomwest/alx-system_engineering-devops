d2b7e37a5d77daf203a648ab6cdafd895b04e69d
wisdomwest <harmonykoome@gmail.com>
2024-01-16 13:11:27 +0300

    update

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,8 @@ def number_of_subscribers(subreddit):
     """queries the Reddit API"""
     about = requests.get("https://www.reddit.com/r/{}/about.json"
                          .format(subreddit),
-                         headers={"User-Agent": "west"})
+                         headers={"User-Agent": "west"},
+                         allow_redirects=False)
     if about.status_code == 404:
         return 0
 
