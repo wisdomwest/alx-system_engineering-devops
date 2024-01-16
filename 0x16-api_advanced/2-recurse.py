6fff198f3b64013f5c06dd37dd356abe50e5a85e
wisdomwest <harmonykoome@gmail.com>
2024-01-16 22:21:12 +0300

    task 2 using recusion

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,1 +1,28 @@
 #!/usr/bin/python3
+"""queries the Reddit API and returns titles
+    of first 10 hot posts for a given subreddit using recursion"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[], after=""):
+    """queries the Reddit API"""
+    hot = requests.get("https://www.reddit.com/r/{}/hot.json"
+                       .format(subreddit),
+                       headers={"User-Agent": "west"},
+                       params={"after": after},
+                       allow_redirects=False)
+    if hot.status_code == 200:
+        for data in hot.json().get("data").get("children"):
+            children = data.get("data")
+            title = children.get("title")
+            hot_list.append(title)
+
+        after = hot.json().get("data").get("after")
+
+        if after is None:
+            return hot_list
+        else:
+            return recurse(subreddit, hot_list, after)
+    else:
+        return None
