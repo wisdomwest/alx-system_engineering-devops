20534aa06bed037cb469f0d4f7f7e603c2c7b22a
wisdomwest <harmonykoome@gmail.com>
2024-01-16 13:08:02 +0300

    task 0 easy

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns the number
+    of subscribers for a given subreddit"""
+
+import requests
+from requests.exceptions import HTTPError
+
+
+def number_of_subscribers(subreddit):
+    """queries the Reddit API"""
+    about = requests.get("https://www.reddit.com/r/{}/about.json"
+                         .format(subreddit),
+                         headers={"User-Agent": "west"})
+    if about.status_code == 404:
+        return 0
+
+    return about.json().get("data").get("subscribers")
